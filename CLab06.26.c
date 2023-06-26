3172f10473395af2287bde03db972a4dc6a15219
hssudusinghe <135609482+hssudusinghe@users.noreply.github.com>
2023-06-26 04:27:55 -0700

    Add files via upload

---
+++ CLab06.26.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /* int number ;
+    int i=1, factorial=1;
+    printf(" Enter an enteger\n");
+    scanf("%d",&number);
+    while(i<=number){
+           factorial*=i;
+           i++;
+    }
+            printf ("factorial is%d",factorial);
+}*/
+
+
+
+{
+   /* int number;
+    int sum=0;
+    printf("enter number:");
+    scanf("%d",&number);
+
+    while(number!=0)
+    {
+    int digit =number%10;
+       sum += digit;
+       number/=10;
+
+    }
+    printf("the output is %d",sum);
+    }*/
+
+
+
+
+
